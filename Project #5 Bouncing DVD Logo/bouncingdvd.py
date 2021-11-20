044a5222d2a871b8c7fc546e964023fc5f8d57af
Joshua Gladwin <joshuajgladwin@gmail.com>
2021-11-20 15:02:12 +0100

    Fixed bug where script would crash when on terminal windows with odd-number dimensions;

--- Project #5 Bouncing DVD Logo/bouncingdvd.py
+++ Project #5 Bouncing DVD Logo/bouncingdvd.py
@@ -18,20 +18,20 @@ except ImportError:
 
 # Set up the constants:
 WIDTH, HEIGHT = bext.size()
-# We can't bring to the last column on Windows without it adding a
+# We can't print to the last column on Windows without it adding a
 # newline automatically, so reduce the width by one:
 WIDTH -= 1
 
 NUMBER_OF_LOGOS = 5  # TODO: Try changing this to 1 or 100.
-PAUSE_AMOUNT  = 0.2  # TODO: Try changing this to 1.0 or 0.0.
+PAUSE_AMOUNT = 0.2  # TODO: Try changing this to 1.0 or 0.0.
 # TODO: Try changing this list to fewer colors:
 COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
-UP_RIGHT    = 'ur'
-UP_LEFT     = 'ul'
-DOWN_RIGHT  = 'dr'
-DOWN_LEFT   = 'dl'
-DIRECTIONS  = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+UP_RIGHT = 'ur'
+UP_LEFT = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT = 'dl'
+DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
 
 # Key names for logo dictionaries:
 COLOR = 'color'
@@ -54,88 +54,88 @@ def main():
             # Make sure X is even so it can hit the corner.
             logos[-1][X] -= 1
 
-        corner_bounces = 0  # Count how many times a logo hits a corner.
-        while True:  # Main program loop.
-            for logo in logos:  # Handle each logo in the logos list.
-                # Erase the logo's current location:
-                bext.goto(logo[X], logo[Y])
-                print('    ', end='')  # TODO: Try commenting this line out.
+    corner_bounces = 0  # Count how many times a logo hits a corner.
+    while True:  # Main program loop.
+        for logo in logos:  # Handle each logo in the logos list.
+            # Erase the logo's current location:
+            bext.goto(logo[X], logo[Y])
+            print('    ', end='')  # TODO: Try commenting this line out.
 
-                original_direction = logo[DIR]
+            original_direction = logo[DIR]
 
-                # See if the logo bounces off the corners:
-                if logo[X] == 0 and logo[Y] == 0:
-                    logo[DIR] = DOWN_RIGHT
-                    corner_bounces += 1
-                elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
-                    logo[DIR] = UP_RIGHT
-                    corner_bounces += 1
-                elif logo[X] == WIDTH - 3 and logo[Y] == 0:
-                    logo[DIR] = DOWN_LEFT
-                    corner_bounces += 1
-                elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
-                    logo[DIR] = UP_LEFT
-                    corner_bounces += 1
+            # See if the logo bounces off the corners:
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = DOWN_RIGHT
+                corner_bounces += 1
+            elif logo[X] == 0 and logo[Y] >= HEIGHT - 1:
+                logo[DIR] = UP_RIGHT
+                corner_bounces += 1
+            elif logo[X] >= WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = DOWN_LEFT
+                corner_bounces += 1
+            elif logo[X] >= WIDTH - 3 and logo[Y] >= HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                corner_bounces += 1
 
-                # See if the logo bounces off the left edge:
-                elif logo[X] == 0 and logo[DIR] == UP_LEFT:
-                    logo[DIR] = UP_RIGHT
-                elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
-                    logo[DIR] = DOWN_RIGHT
+            # See if the logo bounces off the left edge:
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = DOWN_RIGHT
 
-                # See if the logo bounces off the right edge:
-                # (WIDTH - 3 because 'DVD' has 3 letters.)
-                elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
-                    logo[DIR] = UP_LEFT
-                elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
-                    logo[DIR] = DOWN_LEFT
+            # See if the logo bounces off the right edge:
+            # (WIDTH - 3 because 'DVD' has 3 letters.)
+            elif logo[X] >= WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = UP_LEFT
+            elif logo[X] >= WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = DOWN_LEFT
 
-                # See if the logo bounces off the top edge:
-                elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
-                    logo[DIR] = DOWN_LEFT
-                elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
-                    logo[DIR] = DOWN_RIGHT
+            # See if the logo bounces off the top edge:
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] = DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] = DOWN_RIGHT
 
-                # See if the logo bounces off the bottom edge:
-                elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
-                    logo[DIR] = UP_LEFT
-                elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
-                    logo[DIR] = UP_RIGHT
+            # See if the logo bounces off the bottom edge:
+            elif logo[Y] >= HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] = UP_LEFT
+            elif logo[Y] >= HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] = UP_RIGHT
 
-                if logo[DIR] != original_direction:
-                    # Change the color when the logo bounce:
-                    logo[COLOR] = random.choice(COLORS)
+            if logo[DIR] != original_direction:
+                # Change color when the logo bounce:
+                logo[COLOR] = random.choice(COLORS)
 
-                # Move the logo. (X moves by 2 because the terminal
-                # characters are twice as tall as they are wide.)
-                if logo[DIR] == UP_RIGHT:
-                    logo[X] += 2
-                    logo[Y] -= 1
-                elif logo[DIR] == UP_LEFT:
-                    logo[X] -= 2
-                    logo[Y] -= 1
-                elif logo[DIR] == DOWN_RIGHT:
-                    logo[X] += 2
-                    logo[Y] += 1
-                elif logo[DIR] == DOWN_LEFT:
-                    logo[X] -= 2
-                    logo[Y] += 1
+            # Move the logo. (X moves by 2 because the terminal
+            # characters are twice as tall as they are wide.)
+            if logo[DIR] == UP_RIGHT:
+                logo[X] += 2
+                logo[Y] -= 1
+            elif logo[DIR] == UP_LEFT:
+                logo[X] -= 2
+                logo[Y] -= 1
+            elif logo[DIR] == DOWN_RIGHT:
+                logo[X] += 2
+                logo[Y] += 1
+            elif logo[DIR] == DOWN_LEFT:
+                logo[X] -= 2
+                logo[Y] += 1
 
-            # Display number of corner bounces:
-            bext.goto(5, 0)
-            bext.fg('white')
-            print('Corner bounces:', corner_bounces, end='')
+        # Display number of corner bounces:
+        bext.goto(5, 0)
+        bext.fg('white')
+        print('Corner bounces:', corner_bounces, end='')
 
-            for logo in logos:
-                # Draw the logos at their new location:
-                bext.goto(logo[X], logo[Y])
-                bext.fg(logo[COLOR])
-                print('DVD', end='')
+        for logo in logos:
+            # Draw the logos at their new location:
+            bext.goto(logo[X], logo[Y])
+            bext.fg(logo[COLOR])
+            print('DVD', end='')
 
-            bext.goto(0, 0)
+        bext.goto(0, 0)
 
-            sys.stdout.flush()  # Required for bext-using programs.
-            time.sleep(PAUSE_AMOUNT)
+        sys.stdout.flush()  # Required for bext-using programs.
+        time.sleep(PAUSE_AMOUNT)
 
 
 # If this program was run (instead of imported), run the animation:
