29e0c6ff4e65719a8a03f1658465cb49947a9dff
Joshua Gladwin <joshuajgladwin@gmail.com>
2021-12-29 22:03:03 +0100

    Finished Project #15 apb;

---
+++ Projects/Project #15 Deep Cave/README.md
@@ -0,0 +1,25 @@
+# Deep Cave
+___
+_by Al Sweigart_ [al@inventwithpython.com](mailto:al@inventwithpython.com)
+
+An animation of a deep cave that goes forever into the earth.
+
+View the original code [here](https://nostarch.com/big-book-small-python-projects).
+
+**Tags**: _tiny_, _beginner_, _scrolling_, _artistic_
+
+___
+
+## TODO List:
+
+* [x] ~~Try changing `WIDTH` to 10 or 30.~~
+
+Changes the width of the entire board. When this is smaller than the initial `gap_width` and `left_width`, both are reduced until smaller than `WIDTH`, then the program runs as expected.
+
+* [x] ~~Try changing `PAUSE_AMOUNT` to 0 or 1.0.~~
+
+Changes the time between the each step. 
+
+* [x] ~~Try uncommenting out `ln 40-48`.~~
+
+Changes the size of the hole on each step.
---
+++ Projects/Project #15 Deep Cave/deepcave.py
@@ -0,0 +1,47 @@
+"""Deep Cave, by Al Sweigart al@inventwithpython.com
+An animation of a deep cave that goes forever into the earth.
+View the original code at https://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, scrolling, artistic"""
+
+
+import random, sys, time
+
+# Set up the constraints:
+WIDTH = 10
+PAUSE_AMOUNT = 0.01
+
+print('Deep Cave, by Al Sweigart al@inventwithpython.com')
+print('Press Ctrl-C to stop.')
+time.sleep(2)
+
+left_width = 20
+gap_width = 10
+
+while True:
+    # Display the tunnel segment:
+    right_width = WIDTH - gap_width - left_width
+    print(('#' * left_width) + (' ' * gap_width) + ('#' * right_width))
+
+    # Check for Ctrl-C press during the brief pause:
+    try:
+        time.sleep(PAUSE_AMOUNT)
+    except KeyboardInterrupt:
+        sys.exit()  # When Ctrl-C is pressed, end the program.
+
+    # Adjust the left side width:
+    dice_roll = random.randint(1, 6)
+    if dice_roll == 1 and left_width > 1:
+        left_width = left_width - 1  # Decrease left side width.
+    elif dice_roll == 2 and left_width + gap_width < WIDTH - 1:
+        left_width = left_width + 1  # Increase left side width.
+    else:
+        pass  # Do nothing; no change in left side width.
+
+    # Adjust the gap width:
+    dice_roll = random.randint(1, 6)
+    if dice_roll == 1 and gap_width > 1:
+        gap_width = gap_width - 1  # Decrease gap width.
+    elif dice_roll == 2 and left_width + gap_width < WIDTH - 1:
+        gap_width = gap_width + 1  # Increase gap width.
+    else:
+        pass  # Do nothing; no change in gap width.
